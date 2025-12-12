bf706e57a2865172414a89707c2b63777ec6c873
Dan Avner <dan.avner@noirlab.edu>
2025-12-12 09:58:08 -0800

    GOATS-1126: Tracked to having to inject user-aware settings into forms.

--- src/goats_tom/facilities/overrides.py
+++ src/goats_tom/facilities/overrides.py
@@ -9,7 +9,7 @@ from tom_observations.facilities.lco import LCOSettings
 from tom_observations.facilities.soar import SOARFacility as BaseSOARFacility
 from tom_observations.facilities.soar import SOARSettings
 
-log = logging.getLogger(__name__)
+logger = logging.getLogger(__name__)
 
 
 class UserTokenMixin:
@@ -31,6 +31,9 @@ class UserTokenMixin:
         """
         Store the current request user for subsequent token look-ups.
         """
+        logger.debug(
+            "Setting user %r on %s settings", user.username, self.__class__.__name__
+        )
         self._user = user
 
     def get_setting(self, key: str) -> Any:
@@ -40,8 +43,9 @@ class UserTokenMixin:
         if key == "api_key":
             token = self._current_user_token
             if token:
+                logger.debug("Using per-user API token")
                 return token
-
+            logger.debug("No per-user API token found")
         # Otherwise, do the default.
         return super().get_setting(key)
 
@@ -98,11 +102,43 @@ class UserAwareFacilityMixin:
         kwargs.setdefault("facility_settings", self.settings_cls(self.name))
         super().__init__(*args, **kwargs)
 
+        base_settings = getattr(self, "facility_settings", None)
+
+        if base_settings is None:
+            raise RuntimeError(
+                f"{self.__class__.__name__} has no facility_settings after init"
+            )
+
+        # Re-wrap using the same facility name
+        self.facility_settings = self.settings_cls(base_settings.facility_name)
+
     def set_user(self, user) -> None:
         super().set_user(user)
         settings_obj = getattr(self, "facility_settings", None)
         if settings_obj and hasattr(settings_obj, "set_user"):
             settings_obj.set_user(user)
+
+    def get_form(self, observation_type):
+        base_form_cls = super().get_form(observation_type)
+        settings_obj = getattr(self, "facility_settings", None)
+        if settings_obj is None:
+            logger.critical(
+                "%s has no facility settings and this breaks user-aware forms",
+                self.__class__.__name__,
+            )
+
+        class UserAwareForm(base_form_cls):
+            def __init__(self, *args, **kwargs):
+                kwargs["facility_settings"] = settings_obj
+                super().__init__(*args, **kwargs)
+
+        logger.debug(
+            "Created %s (subclass of %s.%s)",
+            UserAwareForm.__name__,
+            base_form_cls.__module__,
+            base_form_cls.__name__,
+        )
+        return UserAwareForm
 
 
 class LCOFacility(UserAwareFacilityMixin, BaseLCOFacility):
@@ -110,6 +146,7 @@ class LCOFacility(UserAwareFacilityMixin, BaseLCOFacility):
     LCO facility with per-user API keys.
     """
 
+    logger.debug("Defining LCOFacility with UserAwareLCOSettings")
     settings_cls = UserAwareLCOSettings
 
 
@@ -118,4 +155,5 @@ class SOARFacility(UserAwareFacilityMixin, BaseSOARFacility):
     SOAR facility with per-user API keys.
     """
 
+    logger.debug("Defining SOARFacility with UserAwareSOARSettings")
     settings_cls = UserAwareSOARSettings
