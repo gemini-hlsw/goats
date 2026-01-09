0ddde636d9affda9979218cf42b450a76841746a
Miguel Gómez <miguel.gomez@noirlab.edu>
2026-01-09 18:12:39 -0300

    GOATS-1137: Make the Blanco button visible as well as operational

--- src/goats_tom/facilities/__init__.py
+++ src/goats_tom/facilities/__init__.py
@@ -1,4 +1,10 @@
 from .gemini import GEMObservationForm, GOATSGEMFacility
-from .overrides import LCOFacility, SOARFacility
+from .overrides import BLANCOFacility, LCOFacility, SOARFacility
 
-__all__ = ["GEMObservationForm", "GOATSGEMFacility", "LCOFacility", "SOARFacility"]
+__all__ = [
+    "GEMObservationForm",
+    "GOATSGEMFacility",
+    "LCOFacility",
+    "SOARFacility",
+    "BLANCOFacility",
+]
